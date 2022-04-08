03c949d00ef3844f32d51f5137685474b1996ff2
Kevin Flores <kevin.flores@mavs.uta.edu>
2022-04-07 22:40:43 -0500

    first commit

---
+++ UniVerse/app/src/main/java/com/example/universe/Feed.java
@@ -0,0 +1,18 @@
+package com.example.universe;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class Feed extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_feed);
+    }
+
+
+}
