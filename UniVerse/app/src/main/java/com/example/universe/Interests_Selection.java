351ec373a89838f7469d5552987c8b9699ce2e03
Joshua Boudria <joshua.boudria@gmail.com>
2022-04-07 20:48:54 -0500

    Fist commit

--- UniVerse/app/src/main/java/com/example/universe/Interests_Selection.java
+++ UniVerse/app/src/main/java/com/example/universe/Interests_Selection.java
@@ -14,7 +14,7 @@ import com.google.firebase.database.ValueEventListener;
 import com.google.firebase.database.core.Context;
 
 public class Interests_Selection extends AppCompatActivity {
-
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
