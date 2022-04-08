3f7ab7bbfb47cc8b4521e143c5e92e608c7afc88
Adam E <EmersonEXE@pm.me>
2022-04-08 11:45:35 -0500

    Student info activity

---
+++ UniVerse/app/src/main/java/com/example/universe/RegisterStudentInfo.java
@@ -0,0 +1,14 @@
+package com.example.universe;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RegisterStudentInfo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register_student_info);
+    }
+}
