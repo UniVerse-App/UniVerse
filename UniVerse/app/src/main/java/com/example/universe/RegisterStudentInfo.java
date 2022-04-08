aea60152c3254b66f20af74939e215fcb7945dd2
Adam E <EmersonEXE@pm.me>
2022-04-08 14:30:15 -0500

    Select profile picture from device

--- UniVerse/app/src/main/java/com/example/universe/RegisterStudentInfo.java
+++ UniVerse/app/src/main/java/com/example/universe/RegisterStudentInfo.java
@@ -1,16 +1,22 @@
 package com.example.universe;
 
-import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.NumberPicker;
 import android.widget.Spinner;
+import android.widget.Toast;
+
+import com.google.android.material.imageview.ShapeableImageView;
 
 import java.lang.reflect.Array;
 import java.time.LocalDate;
@@ -22,6 +28,8 @@ public class RegisterStudentInfo extends AppCompatActivity implements View.OnCli
     private Spinner majorSpinner;
     private NumberPicker gradYear;
     private Button nextButton;
+    private ShapeableImageView selectProfilePic, profilePic;
+    private Integer PICK_IMAGE = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,6 +47,11 @@ public class RegisterStudentInfo extends AppCompatActivity implements View.OnCli
         nextButton = findViewById(R.id.studentRegistrationNextButton);
         nextButton.setOnClickListener(this);
 
+        profilePic = findViewById(R.id.profilePicBackground);
+
+        selectProfilePic = findViewById(R.id.selectProfilePic);
+        selectProfilePic.setOnClickListener(this);
+
 
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.gradArray, android.R.layout.simple_spinner_item);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
@@ -50,6 +63,19 @@ public class RegisterStudentInfo extends AppCompatActivity implements View.OnCli
         if (view.getId() == R.id.studentRegistrationNextButton) {
             nextScreen();
         }
+        else if (view.getId() == R.id.selectProfilePic) {
+           Intent gallery = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.INTERNAL_CONTENT_URI);
+           startActivityForResult(gallery, 3);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_OK && data != null) {
+            Uri selectedImage = data.getData();
+            profilePic.setImageURI(selectedImage);
+        }
     }
 
     private void nextScreen() {
@@ -58,6 +84,8 @@ public class RegisterStudentInfo extends AppCompatActivity implements View.OnCli
 
         if (!major.equals("Choose a major...")) {
             // TODO: Load Interests Activity
+        } else {
+            Toast.makeText(this, "Please select a Field of Study", Toast.LENGTH_SHORT).show();
         }
     }
 }
