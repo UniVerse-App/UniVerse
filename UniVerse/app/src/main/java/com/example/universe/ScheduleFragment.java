fb9a56921c1acfbf83cbf35a253f14ad4ce75ad0
Adam E <EmersonEXE@pm.me>
2022-04-16 13:46:42 -0500

    Base layout of schedule frag

--- UniVerse/app/src/main/java/com/example/universe/ScheduleFragment.java
+++ UniVerse/app/src/main/java/com/example/universe/ScheduleFragment.java
@@ -7,8 +7,11 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 public class ScheduleFragment extends Fragment {
+
+    private TextView todaysDate;
 
     public ScheduleFragment(){
         // require a empty public constructor
@@ -17,6 +20,15 @@ public class ScheduleFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
+        todaysDate = getView().findViewById(R.id.todaysDate);
+        todaysDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
         return inflater.inflate(R.layout.fragment_schedule, container, false);
+
     }
 }
