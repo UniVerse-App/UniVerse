42388558fae5c2eed39ec3d26e114fde20602390
Adam E <EmersonEXE@pm.me>
2022-04-19 13:49:25 -0500

    Datepicker issues

--- UniVerse/app/src/main/java/com/example/universe/ScheduleFragment.java
+++ UniVerse/app/src/main/java/com/example/universe/ScheduleFragment.java
@@ -1,5 +1,7 @@
 package com.example.universe;
 
+import android.app.DatePickerDialog;
+import android.content.Context;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,12 +9,16 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.DatePicker;
 import android.widget.TextView;
+
+import java.util.Calendar;
 
 public class ScheduleFragment extends Fragment {
 
     private TextView todaysDate;
-
+    private Context thisContext;
+    private Calendar myCalendar;
     public ScheduleFragment(){
         // require a empty public constructor
     }
@@ -20,15 +26,49 @@ public class ScheduleFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
-        todaysDate = getView().findViewById(R.id.todaysDate);
+        View rootView = inflater.inflate(R.layout.fragment_schedule, container, false);
+
+        thisContext = container.getContext();
+
+        todaysDate = rootView.findViewById(R.id.todaysDate);
+
+        DatePickerDialog.OnDateSetListener date =new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker view, int year, int month, int day) {
+                myCalendar = Calendar.getInstance();
+                myCalendar.set(Calendar.YEAR, year);
+                myCalendar.set(Calendar.MONTH,month);
+                myCalendar.set(Calendar.DAY_OF_MONTH,day);
+            }
+        };
+
         todaysDate.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                new DatePickerDialog(thisContext,0, date, myCalendar.get(Calendar.YEAR),myCalendar.get(Calendar.MONTH),myCalendar.get(Calendar.DAY_OF_MONTH)).show();
             }
         });
 
-        return inflater.inflate(R.layout.fragment_schedule, container, false);
+        return rootView;
+
+    }
+
+    private void initDatePicker() {
+        DatePickerDialog.OnDateSetListener dateSetListener = new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker datePicker, int year, int month, int day) {
+                month = month + 1;
+                //String date = makeDateString(day,month,year);
+                //dateButton.setText(date);
+            }
+        };
+
+        Calendar cal = Calendar.getInstance();
+        int year = cal.get(Calendar.YEAR);
+        int month = cal.get(Calendar.MONTH);
+        int day = cal.get(Calendar.DAY_OF_MONTH);
+
+        DatePickerDialog todaysDatePicker = new DatePickerDialog(thisContext, 0, dateSetListener, year, month, day);
 
     }
 }
