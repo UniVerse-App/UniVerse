3ead3b4bde40f475bb61159f0570a842009920c2
Adam E <EmersonEXE@pm.me>
2022-04-20 16:56:55 -0500

    Schedule screen loading events from Firebase but not user specific yet

--- UniVerse/app/src/main/java/com/example/universe/Event.java
+++ UniVerse/app/src/main/java/com/example/universe/Event.java
@@ -1,40 +1,74 @@
 package com.example.universe;
 
 public class Event {
+
     private String eventName;
+    private String location;
+    private String time;
+    private long timestamp;
     private String description;
     private String photo;
 
-    public Event(String eventName, String description, String photo) {
+    public Event() {
+
+    }
+
+    public Event(String eventName, String location, String time, long timestamp, String description, String photo) {
+        this.location = location;
         this.eventName = eventName;
+        this.time = time;
+        this.timestamp = timestamp;
         this.description = description;
-        this.photo = photo;
+        this.photo = photo
     }
 
     public String getEventName() {
         return eventName;
     }
 
-    public void setEventName(String eventName) {
-        this.eventName = eventName;
+    public void setEventName(String name) {
+        this.eventName = name;
     }
 
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+
+    public long getTimestamp() {
+        return this.timestamp;
+    }
+
+    public void setTimestamp(long timestamp) {
+        
+        this.timestamp = timestamp;
+    }
+    
     public String getDescription() {
         return description;
     }
-
+    
     public void setDescription(String description) {
         this.description = description;
     }
-
+    
     public String getPhoto() {
         return photo;
     }
-
+    
     public void setPhoto(String photo) {
         this.photo = photo;
     }
-    public Event(){
 
-    }
 }
